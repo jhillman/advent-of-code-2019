51bbfc339ccb81c0a816137843af8f14713c14d3
Jeff Hillman <jeff.hillman@gmail.com>
2020-12-05 20:28:55 -0700

    Day 1

---
+++ day01/part1.c
@@ -0,0 +1,20 @@
+/* Day 1, part 1 = 3465154 */
+
+#include <stdio.h>
+
+int main() {
+    FILE *modulesFile = fopen("modules.txt", "r");
+
+    if (modulesFile) {
+        int mass;
+        int totalFuel = 0;
+
+        while (!feof(modulesFile) && fscanf(modulesFile, "%d", &mass)) {
+            totalFuel += (mass / 3) - 2;
+        }
+
+        printf("%d", totalFuel);
+    }
+
+    return 0;
+}
---
+++ day01/part2.c
@@ -0,0 +1,26 @@
+/* Day 1, part 2 = 5194864 */
+
+#include <stdio.h>
+
+int main() {
+    FILE *modulesFile = fopen("modules.txt", "r");
+
+    if (modulesFile) {
+        int mass;
+        int totalFuel = 0;
+
+        while (!feof(modulesFile) && fscanf(modulesFile, "%d", &mass)) {
+            int fuel = (mass / 3) - 2;
+
+            while (fuel > 0) {
+                totalFuel += fuel;
+                
+                fuel = (fuel / 3) - 2;
+            }
+        }
+
+        printf("%d", totalFuel);
+    }
+
+    return 0;
+}
